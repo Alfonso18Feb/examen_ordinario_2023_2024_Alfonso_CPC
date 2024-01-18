2ce995d1dd9b0d4eb339eb96dd168845a541bc81
Alfonso Criado-Pou Castellanos <146075356+Alfonso18Feb@users.noreply.github.com>
2024-01-18 12:50:04 +0100

    ejercicio 2
    este es la parte que importa los volumenes nombres calificacion y ect y va restando y printeando.
    Eso es o escrito.

---
+++ nombres.py
@@ -0,0 +1,11 @@
+def nombre(nombres):
+    if 1:
+        return nombre[0]
+    elif 2:
+        return nombre [1]
+    else:
+        return nombre[-1]
+int(input('calificacion de estrella:'))
+nombres=['Concordia','Ilum','Kamino']
+apellidos=nombre(nombres)
+print(apellidos)
---
+++ volumen.py
@@ -0,0 +1,10 @@
+def volumen(nombres):
+    if 'Concordia':
+        return 500
+    elif 'Ilum':
+        return 1200
+    else:
+        return 800
+nombres=['Concordia','Ilum','Kamino']
+VidaP=volumen(nombres)
+print(VidaP)
