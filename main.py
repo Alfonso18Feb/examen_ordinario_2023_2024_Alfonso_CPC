503c0bfdb04cab653abca8f8903c393b3acd1950
Alfonso Criado-Pou Castellanos <146075356+Alfonso18Feb@users.noreply.github.com>
2024-01-18 12:52:51 +0100

    Añadir Ajedrez
    temporizador de ajedrez en python espera que movimiento de los otros.
    son muy lentos estos jugadores

---
+++ main.py
@@ -0,0 +1,29 @@
+import time
+class temporizador():
+    def __init__(self,time,nombre,move):
+        self.time=time
+        self.nombre=nombre
+        self.move=move
+    def move(move,time,self):
+        if 'H':
+            return move
+        elif 'C':
+            return move
+        for move in time:
+            if time>60:
+                return time+10 (input('move 1 es H y 2 es Carlson:'))
+            elif time<60:
+                return time+5 (input('move 1 es H y 2 es Carlson:'))
+        if 1:
+            return self.move+1 (input('move 1 es H y 2 es Carlson:'))
+        elif 2:
+            return self.move+1 (input('move 1 es H y 2 es Carlson:'))
+        for i in time:
+            return 180-i
+
+        
+start=str(input('quien empieza escribe en mayuscula:'))
+mover=(input('move 1 es H y 2 es Carlson:'))
+persona1=temporizador(time.sleep(180),'Hikaru',1)
+persona2=temporizador(time.sleep(180),'Carlson',1)
+i=1
